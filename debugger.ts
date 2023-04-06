b111eb1246551caf24ed2fafad5fac3e72c397cb
weiyafei <2553241022@qq.com>
2023-04-06 13:20:03 +0800

    穿件 rollup函数

--- debugger.ts
+++ debugger.ts
@@ -5,11 +5,13 @@
  * @LastEditTime: 2023-04-06 13:15:31
  * @LastEditors: wsy
  */
-// import rollup from './lib/rollup'
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
+import rollup from './lib/rollup'
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
 const entry = path.resolve(dirname, './src/index.ts')
 const output = path.resolve(dirname, './dist/index.js')
+
+rollup(entry, output)
---
+++ lib/rollup.ts
@@ -0,0 +1,10 @@
+/*
+ * @Description:
+ * @Author: wsy
+ * @Date: 2023-04-06 13:16:35
+ * @LastEditTime: 2023-04-06 13:19:14
+ * @LastEditors: wsy
+ */
+function rollup(entry: string, output: string) {}
+
+export default rollup
