1c2cb55386b49bf94107127ce680d2c3f9d03116
weiyafei <2553241022@qq.com>
2023-11-17 21:25:57 +0800

    feat(init): 初始化项目

---
+++ eslint.config.js
@@ -0,0 +1,10 @@
+/*
+ * @Description:
+ * @Author: wsy
+ * @Date: 2023-11-17 21:17:26
+ * @LastEditTime: 2023-11-17 21:17:40
+ * @LastEditors: wsy
+ */
+import antfu from '@antfu/eslint-config'
+
+export default antfu()
---
+++ packages/mini-roullup/src/index.ts
@@ -0,0 +1,7 @@
+/*
+ * @Description:
+ * @Author: wsy
+ * @Date: 2023-11-17 21:20:56
+ * @LastEditTime: 2023-11-17 21:22:47
+ * @LastEditors: wsy
+ */
